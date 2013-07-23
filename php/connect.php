8ba8bddb86b7e77e615191e7dc0d54fb3fd94fc5
Ken Tian <ken.yht@gmail.com>
2013-07-22 21:11:06 -0500

    connection script finished, register script in progress

---
+++ php/connect.php
@@ -0,0 +1,14 @@
+<?php
+	//this script is for connecting to the MySQL database
+	//defined words to make things easy
+	DEFINE ('DB_USER', 'root');
+	DEFINE ('DB_PASSWORD', '');
+	DEFINE ('DB_HOST', 'localhost');
+	DEFINE ('DB_NAME', 'goaldata');
+	//tries to connect to the database, else it screams ERROR and dies
+	$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD) OR
+			die ('Could not connect to MySQL: ' . mysqli_error());
+	//finds the database or else it dies
+	mysqli_select_db(DB_NAME) OR
+			die ('Could not select the database: ' . mysqli_error());
+?>
---
+++ php/register.php
@@ -0,0 +1,10 @@
+<?php
+	if (isset($_POST['submitted'])) {
+		$errors = array();
+		require_once ('connect.php');
+		
+		if () {
+		
+		}
+	}
+?>
