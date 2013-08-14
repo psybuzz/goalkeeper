b4639a1bd8a494b5ffaf6110187cf8cf2aae46cd
Ken Tian <ken.yht@gmail.com>
2013-08-13 22:34:24 -0500

    added log off and remember me

--- php/login.php
+++ php/login.php
@@ -1,18 +1,15 @@
 <?php
-	include("connect.php");
-	
+
 	if (isset($_POST["loginsubmit"])) {
-		session_name("GoalKeeper");
-		//session_set_cookie_params(2 * 7 * 24 * 60 * 60); //cookie lives for 2 weeks
-		session_start();
+
 		$error = array();
+
 		if (!$_POST["loginname"] || !$_POST["loginpass"]) {
 			$error[] = "All fields must be filled in";
 		}
 		
 		$username = mysqli_real_escape_string($con, $_POST["loginname"]);
 		$password = mysqli_real_escape_string($con, $_POST["loginpass"]);
-		//$_POST["rememberME"] = (int)$_POST["rememberME"];
 		
 		$query = "SELECT username, email, password FROM users WHERE username = '$username'";
 		$result = mysqli_query($con, $query);
@@ -25,8 +22,13 @@
 				$_SESSION['user_name'] = $result_row->username;
 				$_SESSION['user_email'] = $result_row->email;
 				$_SESSION['user_logged_in'] = 1;
-				
+				$_SESSION['rememberMe'] = $_POST['rememberME'];
 				$user_is_logged_in = true;
+
+				session_name("GoalKeeper");
+				session_set_cookie_params(2 * 7 * 24 * 60 * 60); //cookie lives for 2 weeks
+				session_start();
+				setcookie('gRemember', $_POST['rememberME']);
 			}
 			else {
 				$error[] = "Wrong password. Try again.";
