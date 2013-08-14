512c350ab1eff34aa65fa43da9b5deb7c46ad0f3
Ken Tian <ken.yht@gmail.com>
2013-08-13 19:06:16 -0500

    changed index.html to index.php, added login/registration dummies

--- php/login.php
+++ php/login.php
@@ -15,8 +15,8 @@
 		//$_POST["rememberME"] = (int)$_POST["rememberME"];
 		
 		$query = "SELECT username, email, password FROM users WHERE username = '$username'";
-		$result->mysqli_query($con, $query);
-		$num->mysqli_num_rows($result);
+		$result = mysqli_query($con, $query);
+		$num = mysqli_num_rows($result);
 		
 		if ($num == 1) {
 			$result_row = mysqli_fetch_object($result);
@@ -29,13 +29,13 @@
 				$user_is_logged_in = true;
 			}
 			else {
-				$error[] = "Wrong password. Try again."
+				$error[] = "Wrong password. Try again.";
 			}
 		}
 		else {
 			$error[] = "This user does not exist";
 		}
-		
+		 
 		if (!empty($error)) {
 			$_SESSION["msg"]["login-err"] = implode("<br />", $error); //save the error messages in the session
 		}
