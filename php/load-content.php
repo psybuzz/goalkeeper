721c7b291ba3158650e3293961a62073e7cafab9
E L <ekluo1@gmail.com>
2013-12-30 21:36:16 -0600

    really old php stuff

--- php/load-content.php
+++ php/load-content.php
@@ -3,13 +3,13 @@
 	$name = mysqli_real_escape_string($con, $_POST['name']);
 	$userId = mysqli_real_escape_string($con, $_POST['userId']);
 
-	$query = "SELECT title, description FROM goals WHERE id = '$userId";
+	$query = "SELECT label, log, uploaded_files FROM content WHERE id = '$userId'";
 	$result = mysqli_query($con, $query);
 
 	$rows = array();
 	while($row = mysqli_fetch_assoc($result)) {
 	    $rows[] = $row;
 	}
-	
+
 	echo json_encode($rows);
 ?>
